17ce1f38d29a78651d6563a47fcbdda1f0b3b96e
Lucas FAGIOLI <125802911+SIIR3X@users.noreply.github.com>
2025-12-05 13:06:34 +0100

    feat(server): ajout du gestionnaire et des parsers de requêtes

---
+++ code/cpp/include/serveur/requetes/parser/distance/i_distance_parser.h
@@ -0,0 +1,22 @@
+#ifndef I_DISTANCE_PARSER_H
+#define I_DISTANCE_PARSER_H
+
+#include "modele/generique/distance.h"
+#include "serveur/requetes/parser/distance/i_distance_parser_base.h"
+#include "serveur/requetes/parser/i_parser_base.h"
+
+/**
+ * @class IDistanceParser
+ * @brief Interface générique pour les parseurs de stratégies de distances de type T.
+ */
+template <typename T>
+class IDistanceParser : public IDistanceParserBase, public IParserBase<T>
+{
+public:
+    /**
+     * @brief Destructeur.
+     */
+    virtual ~IDistanceParser() = default;
+};
+
+#endif  // I_DISTANCE_PARSER_H
